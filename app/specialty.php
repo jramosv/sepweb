0c490bce71712237ac7ea265d26748f95f9af256
Newtella <fl.mlopezgt@gmail.com>
2017-10-12 02:13:09 -0600

    Doctor and Specialty
    creacion de modelos Doctors, specialty con sus respectivos controladores y migraciones.

---
+++ app/specialty.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class specialty extends Model
+{
+
+    protected $fillable = ['nombre'];
+
+    public function Doctores()
+    {
+        return $this->hasMany('App\Doctor','id_especialidad');
+    }
+
+}
+
