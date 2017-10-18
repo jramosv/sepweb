78f6881077808a42482aa44dcfbd7ac289b4b34e
@intelguasoft <hnrdiaz@gmail.com>
2017-10-18 00:39:59 -0600

    nueva actualizacion

--- resources/views/patients/create.blade.php
+++ resources/views/patients/create.blade.php
@@ -1,48 +1,44 @@
-<form method="POST" action="/pacientes">
-<div class="modal fade" id="create_patient" role="dialog">
-	<div class="modal-dialog">
-		<div class="modal-content">
-			<div class="modal-header">
-				<button type="button" class="close" data-dismiss="modal">
-					<span>&times;</span>
-				</button>
-				<h4>Crear paciente</h4>
-			</div>
-			<div class="modal-body">
-				<div class="form-group {{ $errors->has('first_name') ? 'has-error' : '' }}">
-					<label for="first_name">Nombre</label>
-					<input type="text" name="first_name" class="form-control" value="{{ old('first_name') }}" autofocus />
-					@if( $errors->has('first_name'))
-						<span class="help-block">
-							<strong>{{ $errors->first('first_name') }}</strong>
-						</span>
-					@endif
-				</div>
+@extends('layout.admin.admin')
+@section('titulo', 'Crear paciente')
+@section('contenido')
+	<h4>Crear paciente</h4>
 
-				<div class="form-group {{ $errors->has('last_name') ? 'has-error' : '' }}">
-					<label for="last_name">Apellidos</label>
-					<input type="text" name="last_name" class="form-control" value="{{ old('last_name') }}" />
-					@if( $errors->has('last_name'))
-						<span class="help-block">
-							<strong>{{ $errors->first('last_name') }}</strong>
-						</span>
-					@endif
-				</div>
+	<form method="POST" action="/pacientes">
+		{{ csrf_field() }}
+		<div class="form-group {{ $errors->has('first_name') ? 'has-error' : '' }}">
+			<label for="first_name">Nombre</label>
+			<input type="text" name="first_name" class="form-control" value="{{ old('first_name') }}" placeholder="Nombre del paciente" autofocus />
+			@if( $errors->has('first_name'))
+				<span class="help-block">
+					<strong>{{ $errors->first('first_name') }}</strong>
+				</span>
+			@endif
+		</div>
 
-				<div class="form-group {{ $errors->has('date_birth') ? 'has-error' : '' }}">
-					<label for="date_birth">Nacimiento</label>
-					<input type="date" name="date_birth" class="form-control" value="{{ old('date_birth') }}" />
-					@if( $errors->has('date_birth'))
-						<span class="help-block">
-							<strong>{{ $errors->first('date_birth') }}</strong>
-						</span>
-					@endif
-				</div>
+		<div class="form-group {{ $errors->has('last_name') ? 'has-error' : '' }}">
+			<label for="last_name">Apellidos</label>
+			<input type="text" name="last_name" class="form-control" value="{{ old('last_name') }}" placeholder="Apellidos del paciente" />
+			@if( $errors->has('last_name'))
+				<span class="help-block">
+					<strong>{{ $errors->first('last_name') }}</strong>
+				</span>
+			@endif
+		</div>
+
+		<div class="form-group {{ $errors->has('date_birth') ? 'has-error' : '' }}">
+			<label for="date_birth">Nacimiento</label>
+			<input type="date" name="date_birth" class="form-control" value="{{ old('date_birth') }}" placeholder="Fecha de nacimiento" />
+			@if( $errors->has('date_birth'))
+				<span class="help-block">
+					<strong>{{ $errors->first('date_birth') }}</strong>
+				</span>
+			@endif
+		</div>
 
 		<div class="form-group {{ $errors->has('sex') ? 'has-error' : '' }}">
 			<label for="sex">Sexo </label>
 			<br />
-				<label for="sex"><input type="radio" name="sex" value="Masculino" > Masculino</label>
+				<label for="sex"><input type="radio" name="sex" value="Masculino" checked > Masculino</label>
 				<br />
 				<label for="sex"><input type="radio" name="sex" value="Femenino" > Femenino</label>
 			@if( $errors->has('sex'))
@@ -63,22 +59,20 @@
 		</div>
 
 		<div class="form-group {{ $errors->has('blood_types_id') ? 'has-error' : '' }}">
-					<label for="blood_types_id">Tipo de sangre</label>
-					<select name="blood_types_id" class="form-control" >
-						<option value="{{ old('blood_types_id') }}">{{ old('blood_types_id') }}</option>
-					</select>
-					@if( $errors->has('blood_types_id'))
-						<span class="help-block">
-							<strong>{{ $errors->first('blood_types_id') }}</strong>
-						</span>
-					@endif
-				</div>
-				
-			</div>
-			<div class="modal-footer">
-				<input type="submit" class="btn btn-primary" value="Guardar">
-			</div>
+			<label for="blood_types_id">Tipo de sangre</label>
+			<select name="blood_types_id" class="form-control" >
+				<option value="-1">[ Seleccione un tipo ]</option>
+				@foreach($tipos_sangre as $item)
+					<option value="{{ $item->id }}">{{  $item->type }}</option>
+				@endforeach
+			</select>
+			@if( $errors->has('blood_types_id'))
+				<span class="help-block">
+					<strong>{{ $errors->first('blood_types_id') }}</strong>
+				</span>
+			@endif
 		</div>
-	</div>
-</div>
-</form>
+		
+		<input type="submit" class="btn btn-primary" value="Guardar">
+	</form>
+@endsection
