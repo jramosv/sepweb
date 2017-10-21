cd068f5c58976338fa65677ffd88d52c93ca9e52
@intelguasoft <hnrdiaz@gmail.com>
2017-10-20 20:20:36 -0600

    Fix error with delete patiente....
    jejejejjeje, solo pendejadas soy

--- resources/views/layout/admin/admin.blade.php
+++ resources/views/layout/admin/admin.blade.php
@@ -48,14 +48,14 @@
                                 <span class="icon-bar"></span>
                                 <span class="icon-bar"></span>
                             </button>
-                            <a class="navbar-brand" href="#">Dashboard</a>
+                            <a class="navbar-brand" href="#">@yield('titulo', 'Dashboard')</a>
                         </div>
                         <div class="collapse navbar-collapse">
                             <ul class="nav navbar-nav navbar-right">
                                 <li>
                                    <a href="">
                                     <i class="fa fa-user-md" aria-hidden="true"></i>
-                                       Freddy Marroquin { Pediatra/Pedofilo }
+                                       Freddy Marroquin { Pediatra/Ginecologo }
                                     </a>
                                 </li>
                                 <li class="dropdown">
