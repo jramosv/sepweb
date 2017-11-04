3ec4553d7a4e3922c727199dbba2ef7e809e5456
Newtella <fl.mlopezgt@gmail.com>
2017-11-04 05:13:56 -0600

    sistema login

--- config/app.php
+++ config/app.php
@@ -170,6 +170,10 @@ return [
         Collective\Html\HtmlServiceProvider::class,
         Yajra\DataTables\DataTablesServiceProvider::class,
         Barryvdh\DomPDF\ServiceProvider::class,
+        Spatie\Permission\PermissionServiceProvider::class,        
+        Laracasts\Flash\FlashServiceProvider::class,        
+        
+    
 
         /*
          * Application Service Providers...
@@ -232,6 +236,8 @@ return [
         'Html' => Collective\Html\HtmlFacade::class,
         'DataTables' => Yajra\DataTables\Facades\DataTables::class,
         'PDF' => Barryvdh\DomPDF\Facade::class,
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
     ],
 
 ];
